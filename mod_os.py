68034d3bf998c54d0627097bb292d2dd7189c3a2
a2kat <mousedrw@ya.ru>
2020-12-23 19:37:15 +0300

    +bytte stroka

--- mod_os.py
+++ mod_os.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 # os.name  # имя операционной системы
 # os.chdir  # смена текущей директории
 # os.getcwd()  # возвращает текущую директорию
@@ -23,3 +24,17 @@ import sys
 # for i in range(len(sys.path)):
 #	print(sys.path[i])
 
+f = open("lol.txt", "r") #открытие файла
+#f.write("hello\n") # запись в файл
+#f.writelines(['new1\n', 'new2\n']) # запись по строка, принимает список
+#print(f.read()) # возвращает прочитанный файл
+print(f.readlines()) # возвращает список из строк прочитанного
+f.close() # закрывает файл
+
+with open("lol.txt", "r") as f: #альтернативное откртие файлы, который сам закроется
+	for line in f:
+		print(line.replace('\n', ''))
+strokabyt = "Hello МИР".encode('utf-8') # кодировка строки
+print(strokabyt)
+stroka = strokabyt.decode('utf-8')
+print(stroka)
